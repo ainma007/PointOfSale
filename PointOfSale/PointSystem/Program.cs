2318360873a94f915bd7ac906903821738d92bb6
ainma007 <b-sword@outlook.com>
2015-05-08 17:13:04 +0300

    new Folders

--- PointOfSale/PointSystem/Program.cs
+++ PointOfSale/PointSystem/Program.cs
@@ -16,7 +16,7 @@ namespace PointSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
